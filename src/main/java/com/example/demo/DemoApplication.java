729bae6f93755fff4f86d6307249f43e83731e28
takfi <takfir719@gmail.com>
2026-03-26 18:38:40 +0200

    Revert "bad commit"
    This reverts commit 3401a9d6fbb93fd746271c10b2d8a06863cf1217.

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -8,7 +8,6 @@ public class DemoApplication {
 
 	public static void main(String[] args) {
 		System.out.println("hello from branch2 considering branch1");
-		System.out.println("bad commit");
 		SpringApplication.run(DemoApplication.class, args);
 	}
 
